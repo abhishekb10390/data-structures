8ee104adecfe525965db9c330dda2dec84ab7a30
Abhishek B <abhishekb10390@gmail.com>
2017-07-05 20:47:15 -0700

    sprint 1 completed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = Object.create(queueMethods);
+  instanceOf.storage = {};
+  instanceOf.lastNum = 1;
+    return instanceOf;
 };
-
+//
 var queueMethods = {};
 
+  queueMethods.enqueue = function(value) {  
+    var last = this.lastNum;
+    this.storage[last] = value;
+    this.lastNum += 1;
+  };
 
+  queueMethods.dequeue = function() {
+    var firstNum = Object.keys(this.storage)[0];
+    var out = this.storage[firstNum];
+    delete this.storage[firstNum];
+    return out;
+  };
+
+  queueMethods.size = function() {
+    return Object.keys(this.storage).length;
+  }; 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = Object.create(stackMethods);
+  instanceOf.storage = {};
+  instanceOf.lastNum = 1;
+    return instanceOf;
 };
-
+//
 var stackMethods = {};
 
+  stackMethods.push = function(value) {  
+    var last = this.lastNum;
+    this.storage[last] = value;
+    this.lastNum += 1;
+  };
 
+  stackMethods.pop = function() {
+    this.lastNum -= 1;
+    var out = this.storage[this.lastNum];
+    delete this.storage[this.lastNum];
+    return out;
+  };
+
+  stackMethods.size = function() {
+    return Object.keys(this.storage).length;
+  };  
