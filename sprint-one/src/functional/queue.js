defc7a5497a584f10d9d1f33b931a6324a4a795b
Abhishek B <abhishekb10390@gmail.com>
2017-07-05 20:45:28 -0700

    sprint 1 completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,26 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
-
+  var lastCalled = 1;
+  //
   someInstance.enqueue = function(value) {
+   
+    storage[lastCalled] = value;
+    lastCalled += 1;
   };
 
   someInstance.dequeue = function() {
-  };
+  
+    var firstNum = Object.keys(storage)[0];
+    var firstOut = storage[firstNum];
+    delete storage[firstNum];
+    return firstOut;
+  }; 
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
-};
+}; 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var lastNum = 1;
+  //
   someInstance.push = function(value) {
+  
+    storage[lastNum] = value;
+    lastNum += 1;
   };
 
   someInstance.pop = function() {
+    lastNum -= 1;
+    var popped = storage[lastNum];
+    delete storage[lastNum];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+ 
+
+
+
+
+
+
