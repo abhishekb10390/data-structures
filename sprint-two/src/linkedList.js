f34c0dd4218c841220e6667d3a362bade9d865aa
Hack Reactor Student <student@hackreactor.com>
2017-07-06 20:56:06 -0700

    Progress on sprint-two

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
+  
   };
 
   list.removeHead = function() {
+    var outhead = list.head.value;
+    list.head = list.head.next;
+    return outhead;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode.next !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;  
+      }   
+    }
+    return (currentNode.value === target);
   };
 
   return list;
